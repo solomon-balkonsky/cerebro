9966ba43f678c13d741759c252fe01b95fbd4ca6
ja <solomon.balkonsky.git@gmail.com>
2025-06-30 00:43:30 +0300

    Update go.sh

--- go.sh
+++ go.sh
@@ -11,40 +11,74 @@ USERPASS="arch"
 ROOTPASS="root"
 EFILABEL="Cerebro ZFS"
 
-# Partitioning
-sgdisk -Z $DISK
-sgdisk -n 1:0:+1981MiB -t 1:ef00 $DISK
-sgdisk -n 2:0:+64GiB -t 2:bf00 $DISK
-sgdisk -n 3:0:0 -t 3:8200 $DISK
+echo "==> Partitioning disk"
+sgdisk -Z "$DISK"
+sgdisk -n 1:0:+1981MiB -t 1:ef00 "$DISK"
+sgdisk -n 2:0:+419GiB -t 2:bf00 "$DISK"
+sgdisk -n 3:0:0 -t 3:8200 "$DISK"
 
-mkfs.fat -F32 $EFI
-mkswap $SWAP
-swapon $SWAP
+echo "==> Formatting partitions"
+mkfs.fat -F32 "$EFI"
+mkswap "$SWAP"
+swapon "$SWAP"
 
-# Base system install
+echo "==> Loading ZFS module"
+modprobe zfs || true
+
+echo "==> Creating ZFS pool"
+zpool create -f -o ashift=12 \
+  -O compression=lz4 \
+  -O atime=off \
+  -O xattr=sa \
+  -O acltype=posixacl \
+  -O relatime=on \
+  -O mountpoint=none \
+  -O canmount=off \
+  -O devices=off \
+  -m none \
+  rpool "$ROOT"
+
+zfs create -o mountpoint=legacy rpool/ROOT
+mount -t zfs rpool/ROOT /mnt
+
+mkdir -p /mnt/boot
+mount "$EFI" /mnt/boot
+
+echo "==> Updating mirrorlist"
 pacman -Sy --noconfirm archlinux-keyring reflector
-reflector --country "Ukraine,Poland,Moldova,Czech Republic,Hungary,Lithuania,Latvia,Slovenia,Slovakia,Romania,Bulgaria,Croatia,Serbia,South Korea,Singapore,Hong Kong,Switzerland,Denmark,Netherlands,Sweden,United Arab Emirates,Norway,Japan,Finland,Canada,Germany,United Kingdom,France,Belgium,Luxembourg,Israel,Spain,Estonia,Austria,Malaysia,Thailand,Portugal,Ireland,Italy,Australia,Greece,Chile,Uruguay,Qatar,Kuwait,Turkey,Brazil" --latest 24 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+reflector --country "Ukraine,Poland,Moldova,Czech Republic,Hungary,Lithuania,Latvia,Slovenia,Slovakia,Romania,Bulgaria,Croatia,Serbia,South Korea,Singapore,Hong Kong,Switzerland,Denmark,Netherlands,Sweden,United Arab Emirates,Norway,Japan,Finland,Canada,Germany,United Kingdom,France,Belgium,Luxembourg,Israel,Spain,Estonia,Austria,Malaysia,Thailand,Portugal,Ireland,Italy,Australia,Greece,Chile,Uruguay,Qatar,Kuwait,Turkey,Brazil" \
+  --latest 16 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
 
-GNOME_PKGS=$(pacman -Sqg gnome | grep -Ev 'gdm|yelp|epiphany|totem|gnome-weather|gnome-maps|gnome-software|gnome-music|gnome-calendar|simple-scan|gnome-tour|malcontent|gnome-user-docs|decibels|gnome-contacts|gnome-characters|gnome-photos|gnome-font-viewer|gnome-sound-recorder|gnome-boxes|gnome-remote-desktop|gnome-multi-writer|seahorse')
+echo "==> Installing base system"
+GNOME_PKGS=$(pacman -Sqg gnome | grep -Ev 'yelp|epiphany|totem|gnome-weather|gnome-maps|gnome-software|gnome-music|gnome-calendar|simple-scan|gnome-tour|malcontent|gnome-user-docs|decibels|gdm|gnome-characters|gnome-photos|gnome-font-viewer|gnome-sound-recorder|gnome-remote-desktop|gnome-multi-writer|seahorse')
 
 pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware \
   $GNOME_PKGS \
-  zsh zsh-completions sudo git curl efibootmgr ly networkmanager nvidia-dkms nvidia-utils rustup booster mold
+  zsh zsh-completions sudo git curl efibootmgr ly networkmanager \
+  nvidia-dkms nvidia-utils rustup booster mold ninja
 
-rustup default stable
+echo "==> Setting rustup default stable"
+arch-chroot /mnt rustup default stable
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo -e "nameserver 1.1.1.1\nnameserver 9.9.9.9\nnameserver 8.8.8.8\noptions timeout:2 attempts:2 rotate" > /mnt/etc/resolv.conf
+echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 9.9.9.9\noptions timeout:2 attempts:2 rotate" > /mnt/etc/resolv.conf
 
-arch-chroot /mnt bash -e <<EOF
+echo "==> Configuring system in chroot"
+arch-chroot /mnt /bin/bash -e <<EOF
 ln -sf /usr/share/zoneinfo/Europe/Kyiv /etc/localtime
 hwclock --systohc
+
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
 echo "$HOSTNAME" > /etc/hostname
-echo -e "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" > /etc/hosts
+cat <<HOSTS > /etc/hosts
+127.0.0.1 localhost
+::1       localhost
+127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
+HOSTS
 
 useradd -m -G wheel -s /bin/zsh $USERNAME
 echo "$USERNAME:$USERPASS" | chpasswd
@@ -54,8 +88,9 @@ chsh -s /bin/zsh $USERNAME
 
 systemctl enable NetworkManager
 systemctl enable ly
+systemctl enable bluetooth
 
-# pacman config
+# pacman.conf
 cat <<PACMANCONF > /etc/pacman.conf
 [options]
 HoldPkg = pacman glibc
@@ -78,7 +113,7 @@ Include = /etc/pacman.d/mirrorlist
 Include = /etc/pacman.d/mirrorlist
 PACMANCONF
 
-# paru config
+# paru.conf
 cat <<PARUCONF > /etc/paru.conf
 [options]
 PgpFetch
@@ -105,19 +140,20 @@ RUSTCONF
 EOF
 
 PARTUUID=$(blkid -s PARTUUID -o value $ROOT)
-arch-chroot /mnt efibootmgr -c -d $DISK -p 1 \
+arch-chroot /mnt efibootmgr -c -d "$DISK" -p 1 \
   -L "$EFILABEL" \
   -l '\\vmlinuz-linux-zen' \
   -u "zfs=rpool/ROOT rw root=PARTUUID=$PARTUUID initrd=\\booster-linux-zen.img resume=$SWAP quiet loglevel=0 mitigations=off noibrs noibpb nospec_store_bypass_disable l1tf=off pcie_aspm=off nvme_core.default_ps_max_latency_us=0 u.random.trust_cpu=on processor.max_cstate=1 nohz=on iommu=pt nvidia_drm.modeset=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1 liburing.force_io_uring=1 zswap.enabled=0"
 
-arch-chroot /mnt bash -e <<EOF
+echo "==> Installing Paru and ZFS AUR packages"
+arch-chroot /mnt /bin/bash -e <<EOF
 cd /home/$USERNAME
 git clone https://aur.archlinux.org/paru.git
 chown -R $USERNAME:$USERNAME paru
 cd paru
 sudo -u $USERNAME makepkg -si --noconfirm
 
-sudo -u $USERNAME paru -S --noconfirm zfs-dkms zfs-utils
+sudo -u $USERNAME paru -Sy --noconfirm zfs-dkms zfs-utils
 EOF
 
-echo "✅ Installation complete."
+echo "✅ Done! Reboot and enjoy $EFILABEL"
